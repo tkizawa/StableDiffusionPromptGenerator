66f222923242333bbb08f0500d6cfc6a4da6e0fc
Tomokazu Kizawa <kizawa@woodstream.jp>
2024-09-27 20:31:55 +0900

    起動用バッチファイルを作成

---
+++ run_StableDiffusionPromptGenerator.bat
@@ -0,0 +1,2 @@
+@echo off
+start "" "C:/Users/tomok/AppData/Local/Microsoft/WindowsApps/pythonw.exe" "C:\Users\tomok\OneDrive\Script\Stable Diffusion Prompt Generator\StableDiffusionPromptGenerator.py"
